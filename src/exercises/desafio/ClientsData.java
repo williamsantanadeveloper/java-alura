0a0374a3780a0292aba4fb2ab42ad96d94c469f5
William Santana <williamandrade1058@gmail.com>
2024-02-15 11:16:48 -0300

    bank account challenge

---
+++ src/exercises/desafio/ClientsData.java
@@ -0,0 +1,53 @@
+package exercises.desafio;
+import java.util.Scanner;
+public class ClientsData {
+    public static void main(String[] args) {
+        System.out.println("***********************");
+
+        System.out.println("Dados iniciais do cliente: ");
+        Scanner reading = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String name = reading.nextLine();
+        System.out.print("Tipo de conta: ");
+        String typeAccount = reading.nextLine();
+        System.out.print("Saldo inicial: ");
+        double openingBalance = reading.nextDouble();
+        String menssageClientsData = String.format(
+                "Nome: %s \n"+
+                "Tipo de conta: %s \n"
+        +       "Saldo inicial: %f", name, typeAccount, openingBalance);
+        System.out.println("***********************");
+        System.out.println(menssageClientsData);
+
+        System.out.println("\nOperações");
+        System.out.println("1- Consultar saldos");
+        System.out.println("2- Receber valor");
+        System.out.println("3- Transferir valor");
+        System.out.println("4- Sair");
+
+        System.out.println("Digite a opção desejada: ");
+        int option = reading.nextInt();
+
+        while (option != 4) {
+            if (option == 1) {
+                System.out.println("Saldo atual: " + openingBalance);
+                break;
+            } else if (option == 2) {
+                System.out.print("Informe o valor que vai receber: ");
+                double receiveValue = reading.nextDouble();
+                double currentBalance = receiveValue + openingBalance;
+                System.out.println("Saldo atualizado R$ " + currentBalance);
+                break;
+            } else if(option == 3) {
+                double removingValue = reading.nextDouble();
+                double currentBalance = openingBalance - removingValue;
+                if (removingValue > openingBalance) {
+                    System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                }
+                System.out.println("Saldo atualizado R$ " + currentBalance);
+                break;
+            }
+            break;
+        }
+    }
+}
